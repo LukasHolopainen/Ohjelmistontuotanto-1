ab6c2160015715c4a73718b1a0963a5d569a6c58
Eroantti <antti.eronenn@gmail.com>
2025-05-12 19:08:00 +0300

    Add files via upload
    Versio 2

--- mokki_tietokanta.sql
+++ mokki_tietokanta.sql
@@ -34,6 +34,12 @@ hinta FLOAT,
 FOREIGN KEY (varausID) REFERENCES varaus(varausID)
 );
 
+INSERT INTO mokki (mokkiID, mokkiNimi, nukkumaPaikat, huoneet, osoite, hintaPerPaiva) VALUES (000, 'Revontulen Ratsastaja', 6, 3, 'Katukujantie 99', 98.76);
 INSERT INTO mokki (mokkiID, mokkiNimi, nukkumaPaikat, huoneet, osoite, hintaPerPaiva) VALUES (001, 'Villa Crocodilo', 8, 5, 'Skibidinkatu 420', 199.99);
 INSERT INTO mokki (mokkiID, mokkiNimi, nukkumaPaikat, huoneet, osoite, hintaPerPaiva) VALUES (002, 'Tralaleron Tila', 10, 8, 'Metsämökkitie 33', 299.95);
 INSERT INTO mokki (mokkiID, mokkiNimi, nukkumaPaikat, huoneet, osoite, hintaPerPaiva) VALUES (003, 'Cocofanton Cartano',  4, 4, 'Kappucinokuja 1', 79.99);
+INSERT INTO mokki (mokkiID, mokkiNimi, nukkumaPaikat, huoneet, osoite, hintaPerPaiva) VALUES (004, 'Meemimökki', 2, 1, 'Kauppiaankatu 22', 14.99);
+
+INSERT INTO asiakas (asiakasID, etuNimi, sukuNimi, puhelinNumero, sahkoPostiOsoite, osoite) VALUES (111, 'Epa', 'Esimerkki', 0401231234, 'epa.esimerkki@mail.com', 'Kissankatu 7');
+INSERT INTO asiakas (asiakasID, etuNimi, sukuNimi, puhelinNumero, sahkoPostiOsoite, osoite) VALUES (222, 'Maija', 'Maijanen', 0402342345, 'maija.maija@mail.com', 'Vilkkuväylä 99');
+INSERT INTO asiakas (asiakasID, etuNimi, sukuNimi, puhelinNumero, sahkoPostiOsoite, osoite) VALUES (333, 'Seppo', 'Salainen', 0403453456, 'sexisepi@mail.com', 'Kaunolanraitti 3');
