6bd57e605ee3a0ea59eba9b1b63159985fc5bd69
x-miroslav-x <mirko.kaukonen03@gmail.com>
2025-05-13 19:30:21 +0300

    Delete lopputyo directory

--- lopputyo/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.button.lopputyo","l":"com.button.lopputyo"}];updateSearchResults();
--- lopputyo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.button.lopputyo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.button.lopputyo to javafx.fxml;
-    exports com.button.lopputyo;
-}
